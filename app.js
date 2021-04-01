ebe3985a9ec6027122e8486a99e6050fc0d60d98
nicozini <nicolasmartinzini@outlook.com>
2021-04-01 14:59:18 -0300

    Nicolas Zini - Modificaciones en header y alta formulario registro

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ app.listen(port, () => {
     console.log('Example app listening on port '+ port);
 });
 
+
+// -------- Rutas -------- //
+
+//Home
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './views/home.html'));
+// });
+
+//Registro
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, './views/register.html'));
 });
+
+//Inicio de sesión
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './views/login.html'));
+// });
