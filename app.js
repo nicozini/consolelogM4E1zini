bb82c3ed0d3b2b776233cce674581fcac9aeb87a
nicozini <nicolasmartinzini@outlook.com>
2021-04-05 19:20:07 -0300

    Nico Zini - Servidor. Heroku. Procfile.

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express')
 const app = express()
 
-const port = 3000;
+const port = process.env.PORT;
 
 const path = require('path');
 app.use(express.static('public'));
 
 
-app.listen(port, () => {
-    console.log('Example app listening on port '+ port);
-});
+// Puerto Express
+// app.listen(port, () => {
+//     console.log('Example app listening on port '+ port);
+// })
+
+// Puerto Heroku
+app.listen(port || 3000, function() {
+    console.log('Example app listening on port ' + port);    
+})
+
 
 
 // -------- Rutas -------- //
-
 //Home
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
